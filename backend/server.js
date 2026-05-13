1f027bf6a12e222c73f722dc4f9e42bd4e6a84f3
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2026-05-13 19:09:34 +0530

    CORS FIX

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://class-plus-assignment-three.vercel.app/"
+  "https://class-plus-assignment-three.vercel.app"
 ].filter(Boolean);
 
 app.use(cors({
